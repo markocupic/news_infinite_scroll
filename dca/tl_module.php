6fba497d34faf7892babaf85158efb105e267b4c
markocupic <m.cupic@gmx.ch>
2017-02-09 14:41:46 +0100

    dev

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -12,4 +12,4 @@
 /**
  * Add palettes to tl_module
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist_infinite_scroll']    = '{title_legend},name,headline,type;{config_legend},news_archives,numberOfItems,news_featured,perPage,skipFirst;{template_legend:hide},news_metaFields,news_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['newslist_infinite_scroll'] = '{title_legend},name,headline,type;{config_legend},news_archives,numberOfItems,news_featured,skipFirst;{template_legend:hide},news_metaFields,news_template,customTpl;{image_legend:hide},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
